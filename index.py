b469a1291f72246f11259d5534dc32c915a7d3c0
daniyar2011 <daniyarmaratbekov2011@gmail.com>
2024-12-01 10:26:59 +0600

    создали новый файл питона
    тема словари, список и кортедж

---
+++ index.py
@@ -0,0 +1,37 @@
+frukti = ["банан", "яблоко", "мандарин"]
+print(frukti)
+
+colors = ("красный", "белай", "зеленый")
+print(colors)
+print(colors[2])
+
+students = {   # словарь
+    "Алиса": 16,
+    "siri": 23,
+    "siri": 14,    #ключи повторятся не должны
+    "Максим": 43   #если будут повторятся то будет показыватся только последний
+}
+
+students["Максим"] = 30
+
+del students["Алиса"]
+
+print(students)
+
+print(students["siri"])
+
+название словаря = {
+ключ: значение
+ключ: значение
+}
+
+frukti = {
+    "Apple": "red",
+    "Orange": "orange",
+    "Banana": "yellow",
+}
+
+frukti["Apple"] = "green"
+del frukti["Orange"]
+
+print(frukti)
